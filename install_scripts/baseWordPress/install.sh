e8e7b7dcd3d61a181089fbb515ce51e687f40b3f
Jonathan Robles <jonathanjarobles@gmail.com>
2017-08-24 15:11:57 -0400

    newest

--- install_scripts/baseWordPress/install.sh
+++ install_scripts/baseWordPress/install.sh
@@ -1,22 +1,13 @@
+PROJECT="baseWordPress"
+
 cp -rf ./_*.sh ../../
 
-rm -rf ../../TEMP/baseWordPress
-git clone https://superjonbot@bitbucket.org/superjonbot/basewordpress.git ../../TEMP/baseWordPress
+rm -rf ../../TEMP/$PROJECT
+git clone https://superjonbot@bitbucket.org/superjonbot/basewordpress.git ../../TEMP/$PROJECT
 
-cd ../../TEMP/baseWordPress
+cd ../../TEMP/$PROJECT
 ls
 cp -rf haproxy ../../SOURCE/
 cp -rf html ../../SOURCE/
 cp -rf sql ../../SOURCE/
 cp -rf node ../../node_docker/
-
-# 
-
-# rm -rf haproxy
-# rm -rf html
-# rm -rf sql
-# rm -rf node
-
-# rm -rf ./*.sh
-
-# cd .. 
